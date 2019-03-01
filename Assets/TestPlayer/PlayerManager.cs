fabaa3d3cfe47ed94d66226aefcecfb4bdfb27e2
Jordan <jordanclark.96@hotmail.com>
2019-03-01 15:02:25 +1300

    mergeing scenes

---
+++ Assets/TestPlayer/PlayerManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour {
+
+    public CameraScript camScript;
+    public KeyCode SwitchChar;
+    public GameObject switchEffect;
+    private Rigidbody rb;
+    private float accel = 12;
+    private float maxSpeed = 600;
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate() {
+        float speed = rb.velocity.magnitude;
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        if (movement != Vector3.zero) {
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement.normalized), 0.15f);
+        }
+        rb.velocity = Vector3.Lerp(rb.velocity, movement * maxSpeed * Time.deltaTime, accel);
+        Ray ray;
+        RaycastHit hit;
+        ray = new Ray(transform.position, transform.forward);
+        if (Physics.Raycast(ray, out hit, 3.0f) && hit.collider.tag == "Character") {
+            if (Input.GetKeyDown(SwitchChar)) {
+                hit.collider.gameObject.GetComponent<PlayerManager>().enabled = true;
+                switchEffect.transform.position = this.transform.position;
+                switchEffect.SetActive(true);
+                SwitchManager.Instance.newTarget = hit.transform;
+                StartCoroutine(SwitchManager.Instance.Switch());
+                camScript.target = hit.transform;
+                this.GetComponent<PlayerManager>().enabled = false;
+            }
+        }
+        Debug.DrawRay(transform.position, transform.forward * 3);
+    }
+}
---
+++ Assets/WizardCode.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WizardCode : MonoBehaviour {
+
+    public GameObject[] greenBlocks;
+    public GameObject[] blueBlocks;
+    public GameObject[] redBlocks;
+
+    public bool redDown, blueDown, greenDown;
+
+    // Use this for initialization
+    void Start()
+{
+
+}
+
+// Update is called once per frame
+void Update()
+{
+    if (Input.GetKeyDown(KeyCode.R) && redDown == false)
+        {
+            for (int i = 0; i < redBlocks.Length; i++)
+            {
+                redBlocks[i].transform.position += new Vector3(0, 2.63f, 0);
+                blueBlocks[i].transform.position += new Vector3(0, -2.63f, 0);
+                
+            }
+            redDown = true;
+            blueDown = false;
+            
+        }
+
+        if (Input.GetKeyDown(KeyCode.B) && blueDown == false)
+        {
+            for (int i = 0; i < blueBlocks.Length; i++)
+            {
+                blueBlocks[i].transform.position += new Vector3(0, 2.63f, 0);
+                greenBlocks[i].transform.position += new Vector3(0, -2.63f, 0);
+                
+            }
+            blueDown = true;
+            greenDown = false;
+            
+        }
+
+        if (Input.GetKeyDown(KeyCode.G) && greenDown == false)
+        {
+            for (int i = 0; i < greenBlocks.Length; i++)
+            {
+                greenBlocks[i].transform.position += new Vector3(0, 2.63f, 0);
+                redBlocks[i].transform.position += new Vector3(0, -2.63f, 0);
+               
+            }
+            greenDown = true;
+            redDown = false;
+            
+
+        }
+
+
+
+
+
+
+    }
+}
