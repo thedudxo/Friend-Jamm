783d42d02a94a3c5d9d0a786f6b2b4d22404c2ef
Jordan <jordanclark96@hotmail.com>
2019-03-04 16:59:27 +1300

    BUTTON

---
+++ Assets/ButtonScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour {
+
+    public bool isPressed = false;
+    public List<Animator> hydrolicPress;
+    
+    private void OnCollisionEnter(Collision collision) {
+        Debug.Log("Collided with " + collision.gameObject);
+        foreach (Animator anim in hydrolicPress) {
+            anim.SetBool("isPressed", true);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision) {
+        foreach (Animator anim in hydrolicPress) {
+            anim.SetBool("isPressed", false);
+        }
+    }
+}
