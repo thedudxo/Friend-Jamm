c996d83c176946807a8f1b2f6bd82aedb652cdc5
Jordan <jordanclark.96@hotmail.com>
2019-03-22 14:10:20 +1300

    Switcher and death trigger

---
+++ Assets/Project/Scripts/DeathTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathTrigger : MonoBehaviour {
+    
+	void Start () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other) {
+        StartCoroutine(PlayerManager.Instance.Death());
+    }
+}
